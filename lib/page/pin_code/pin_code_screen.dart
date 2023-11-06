adaf49a334a6e3ada3233356bca1c61f753913e2
Nahtawat Piyapongpaisarn <nahtawatpiyapongpaisarn@ACNAHTAWATN1.local>
2023-11-06 15:49:58 +0700

    Add pin code controller

--- lib/page/pin_code/pin_code_screen.dart
+++ lib/page/pin_code/pin_code_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do_test/common/size_config.dart';
 import 'package:to_do_test/page/home/home.dart';
+import 'package:to_do_test/page/pin_code/pincode_controller.dart';
 import 'package:to_do_test/provider/sf_provider.dart';
 import 'package:to_do_test/provider/timeout_provider.dart';
 
@@ -14,12 +15,13 @@ class PinCodeScreen extends StatefulWidget {
 }
 
 class _PinCodeScreen extends State<PinCodeScreen> with WidgetsBindingObserver {
-  String enteredPin = '';
+  late PinCodeController pinCodeController;
   SFProvider sfProvider = SFProvider();
 
   @override
   void initState() {
     super.initState();
+    pinCodeController = PinCodeController(context);
     WidgetsBinding.instance.addObserver(this);
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       context.read<TimeoutProvider>().stop();
@@ -35,9 +37,28 @@ class _PinCodeScreen extends State<PinCodeScreen> with WidgetsBindingObserver {
   }
 
   @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    switch (state) {
+      case AppLifecycleState.resumed:
+        print('resumed');
+        break;
+      case AppLifecycleState.inactive:
+        print('inactive');
+        break;
+      case AppLifecycleState.paused:
+        print('paused');
+        break;
+      case AppLifecycleState.detached:
+        print('detached');
+        break;
+    }
+  }
+
+  @override
   void dispose() {
     super.dispose();
-    // context.read<TimeoutProvider>().dispose();
   }
 
   @override
@@ -45,158 +66,140 @@ class _PinCodeScreen extends State<PinCodeScreen> with WidgetsBindingObserver {
     return Scaffold(
       backgroundColor: Color.fromRGBO(235, 237, 253, 1.0),
       body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: defaultSize * 2.4),
-          physics: const BouncingScrollPhysics(),
-          children: [
-            SizedBox(height: defaultSize * 10.0),
-            Center(
-              child: Text(
-                  'This is just sample UI \n Open to create your style :D',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF757575))),
-            ),
-            SizedBox(height: defaultSize * 5.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(6, (index) {
-                return Container(
-                  margin: EdgeInsets.all(defaultSize * 1.6),
-                  width: defaultSize * 1.6,
-                  height: defaultSize * 1.6,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: index < enteredPin.length
-                          ? Colors.grey.shade700
-                          : Colors.grey.withOpacity(0.3)),
-                );
-              }),
-            ),
-            Container(
-              alignment: Alignment.bottomCenter,
-              padding: EdgeInsets.only(
-                top: defaultSize * 1.6,
-                bottom: defaultSize * 3.2,
-              ),
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      height: defaultSize * 2.3,
+        child: ChangeNotifierProvider.value(
+          value: pinCodeController,
+          child: Consumer<PinCodeController>(
+            builder: (context, con, _) {
+              return ListView(
+                padding: EdgeInsets.symmetric(horizontal: defaultSize * 2.4),
+                physics: const BouncingScrollPhysics(),
+                children: [
+                  SizedBox(height: defaultSize * 10.0),
+                  Center(
+                    child: Text(
+                        'This is just sample UI \n Open to create your style :D',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF757575))),
+                  ),
+                  SizedBox(height: defaultSize * 5.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(6, (index) {
+                      return Container(
+                        margin: EdgeInsets.all(defaultSize * 1.6),
+                        width: defaultSize * 1.6,
+                        height: defaultSize * 1.6,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: index < con.enteredPin.length
+                                ? Colors.grey.shade700
+                                : Colors.grey.withOpacity(0.3)),
+                      );
+                    }),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomCenter,
+                    padding: EdgeInsets.only(
+                      top: defaultSize * 1.6,
+                      bottom: defaultSize * 3.2,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        NumpadButton(
-                          text: '1',
-                          onPressed: () async => await setValue('1'),
-                        ),
-                        NumpadButton(
-                          text: '2',
-                          onPressed: () async => await setValue('2'),
-                        ),
-                        NumpadButton(
-                          text: '3',
-                          onPressed: () async => await setValue('3'),
-                        ),
-                      ],
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            height: defaultSize * 2.3,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              NumpadButton(
+                                text: '1',
+                                onPressed: () async => await con.setValue('1'),
+                              ),
+                              NumpadButton(
+                                text: '2',
+                                onPressed: () async => await con.setValue('2'),
+                              ),
+                              NumpadButton(
+                                text: '3',
+                                onPressed: () async => await con.setValue('3'),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: defaultSize * 1.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              NumpadButton(
+                                text: '4',
+                                onPressed: () async => await con.setValue('4'),
+                              ),
+                              NumpadButton(
+                                text: '5',
+                                onPressed: () async => await con.setValue('5'),
+                              ),
+                              NumpadButton(
+                                text: '6',
+                                onPressed: () async => await con.setValue('6'),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: defaultSize * 1.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              NumpadButton(
+                                text: '7',
+                                onPressed: () async => await con.setValue('7'),
+                              ),
+                              NumpadButton(
+                                text: '8',
+                                onPressed: () async => await con.setValue('8'),
+                              ),
+                              NumpadButton(
+                                text: '9',
+                                onPressed: () async => await con.setValue('9'),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: defaultSize * 1.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Container(
+                                height: defaultSize * 7.0,
+                                width: defaultSize * 7.0,
+                              ),
+                              NumpadButton(
+                                text: '0',
+                                onPressed: () async => await con.setValue('0'),
+                              ),
+                              NumpadButton(
+                                onPressed: () async => await con.deleteValue(),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                    SizedBox(
-                      height: defaultSize * 1.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        NumpadButton(
-                          text: '4',
-                          onPressed: () async => await setValue('4'),
-                        ),
-                        NumpadButton(
-                          text: '5',
-                          onPressed: () async => await setValue('5'),
-                        ),
-                        NumpadButton(
-                          text: '6',
-                          onPressed: () async => await setValue('6'),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: defaultSize * 1.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        NumpadButton(
-                          text: '7',
-                          onPressed: () async => await setValue('7'),
-                        ),
-                        NumpadButton(
-                          text: '8',
-                          onPressed: () async => await setValue('8'),
-                        ),
-                        NumpadButton(
-                          text: '9',
-                          onPressed: () async => await setValue('9'),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: defaultSize * 1.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Container(
-                          height: defaultSize * 7.0,
-                          width: defaultSize * 7.0,
-                        ),
-                        NumpadButton(
-                          text: '0',
-                          onPressed: () async => await setValue('0'),
-                        ),
-                        NumpadButton(
-                          onPressed: () async => await deleteValue(enteredPin),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+                  ),
+                ],
+              );
+            }
+          ),
         ),
       ),
     );
-  }
-
-  setValue(String val) async {
-    setState(() {
-      if (enteredPin.length < 6) {
-        enteredPin += val;
-      }
-    });
-
-    if (enteredPin.length == 6 && enteredPin == '123456') {
-      sfProvider.addStringToSF(SFProvider.sfPinCodeKey, enteredPin);
-      await Future.delayed(const Duration(milliseconds: 300));
-      Navigator.pushAndRemoveUntil(
-          context,
-          MaterialPageRoute(builder: (context) => HomePage()),
-          (route) => false);
-    }
-  }
-
-  deleteValue(String val) async {
-    if (val.length > 0) {
-      setState(() {
-        enteredPin = val.split('').sublist(0, val.length - 1).join('');
-      });
-    }
   }
 }
 
---
+++ lib/page/pin_code/pincode_controller.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_test/page/home/home.dart';
+import 'package:to_do_test/provider/sf_provider.dart';
+
+class PinCodeController extends ChangeNotifier {
+  PinCodeController(this.context);
+  SFProvider sfProvider = SFProvider();
+
+  final BuildContext context;
+  String enteredPin = '';
+
+  setValue(String val) async {
+    if (enteredPin.length < 6) {
+      enteredPin += val;
+      notifyListeners();
+    }
+
+    if (enteredPin.length == 6 && enteredPin == '111111') {
+      sfProvider.addStringToSF(SFProvider.sfPinCodeKey, enteredPin);
+      await Future.delayed(const Duration(milliseconds: 300));
+      Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+              (route) => false);
+    }
+  }
+
+  deleteValue() async {
+    if (enteredPin.length > 0) {
+      enteredPin = enteredPin.split('').sublist(0, enteredPin.length - 1).join('');
+      notifyListeners();
+    }
+  }
+}
